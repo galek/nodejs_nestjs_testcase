21f2a6c0bfd534552578478fea217ea87483d8c5
Nick Galko <nickolay.galko@gmail.com>
2020-01-29 16:46:07 +0300

    Добавлена позиция

--- src/vote.service.ts
+++ src/vote.service.ts
@@ -1,12 +1,12 @@
-﻿﻿import { AppService } from "./app.service";
+﻿import { AppService } from "./app.service";
 import { Controller, Body, Post, Get, ValidationPipe, Req, Injectable } from "@nestjs/common";
 import { PayLoadObject, AbstactToken, AbstactKey, ResponseObject } from "./interfaces";
 import * as rawbody from 'raw-body';
 
 @Injectable()
 export class DBDriver {
-  votesArray: Array<{ name: string, votes: number }>
-    = new Array<{ name: string, votes: number }>();
+  votesArray: Array<{ name: string, votes: number, position: number }>
+    = new Array<{ name: string, votes: number, position: number }>();
 
   writeToDB(value: string): ResponseObject {
     if (!value) return { success: false };
@@ -19,8 +19,19 @@ export class DBDriver {
     //   if (a.votes < b.votes) { return 1; }
     //   return 0;
     // });
+
+    // TODO:Errors and borderline cases must be taken into account
+    // BUG: Частный случай: - когда votes одинаковы, отрабатывается первый (надо сделать по дате)
     this.votesArray.sort((a, b) => {
-      return a.votes - b.votes;
+      return b.votes - a.votes;
+    });
+
+    // Позиция не может быть отрицательной
+    let index: number = 1;
+
+    this.votesArray.forEach(element => {
+      element.position = index;
+      index++;
     });
 
     return this.votesArray;
@@ -38,7 +49,8 @@ export class DBDriver {
       return { success: true };
     }
     else {
-      this.votesArray.push({ name: value, votes: 1 });
+      // Позиция не определена еще
+      this.votesArray.push({ name: value, votes: 1, position: 0 });
       return { success: true };
     }
 
@@ -72,12 +84,12 @@ export class VoteController {
       // body is ignored by NestJS -> get raw body from request
       const raw = await rawbody(req);
       const text = raw.toString().trim();
-      console.log('body is readable:', text);
+      if (console) console.log('body is readable:', text);
       // TODO: Почитай, что это
 
     } else {
       // body is parsed by NestJS
-      console.log('data not readable:', data);
+      if (console) console.log('data not readable:', data);
       return this.dbDriver.writeToDB(data.voteFor);
 
     }
