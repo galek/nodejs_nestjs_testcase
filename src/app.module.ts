35741c4931d681c18cc1d5929c2c49428743b9d1
Nick Galko <nikolay.galko@gmail.com>
2020-02-05 19:40:33 +0300

    MongoDB start of implementation

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,13 +9,20 @@ import { PassportModule } from '@nestjs/passport';
 import { UsersModule } from './users/users.module';
 import { DBDriver, VoteController, Results } from './vote.service';
 
+import { MongooseModule } from '@nestjs/mongoose';
+import { Config } from './config';
+
 @Module({
   controllers: [AppController, VoteController, Results],
   imports: [UsersModule, AuthModule,
     JwtModule.register({
       secret: 'secret',
       signOptions: { expiresIn: '60s' }
-    }), PassportModule],
+    }), PassportModule,
+
+    MongooseModule.forRoot('mongodb://' + Config.dbAddress)
+
+  ],
   providers: [AppService, DBDriver],
 })
 export class AppModule { }
