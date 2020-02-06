1d72a0258a59c071546db8141eebcb47465ec1df
Nick Galko <nikolay.galko@gmail.com>
2020-02-06 16:11:16 +0300

    clean

--- opencollective.cmd
+++
@@ -1,7 +0,0 @@
-@IF EXIST "%~dp0\node.exe" (
-  "%~dp0\node.exe"  "%~dp0\node_modules\@nuxtjs\opencollective\src\cjs.js" %*
-) ELSE (
-  @SETLOCAL
-  @SET PATHEXT=%PATHEXT:;.JS;=;%
-  node  "%~dp0\node_modules\@nuxtjs\opencollective\src\cjs.js" %*
-)
