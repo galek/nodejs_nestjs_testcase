e61be8fc8ff8b399c2f4130891d1ba2367c13ac1
Nick Galko <nikolay.galko@gmail.com>
2020-02-12 10:04:36 +0300

    Некорректно указал url в заголовке. (Есть сборка с поддержкой nuxt, это для админки. Смотри соседнюю ветку)

--- src/auth/constants.ts
+++ src/auth/constants.ts
@@ -1,7 +1,7 @@
 /* Copyright (C) 2009-2020, Nick Galko. All rights reserved.
 *
 * This file is part of the Nick Galko source-code
-* (http://https://galek.github.io/portfolio/).
+* (https://galek.github.io/portfolio/).
 *
 * Your use and or redistribution of this software in source and / or
 * binary form, with or without modification, is subject to: (i) your
@@ -9,7 +9,7 @@
 * the Nick Galko License Agreement; and (ii) your inclusion of this notice
 * in any version of this software that you use or redistribute.
 * A copy of the NGTech License Agreement is available by contacting
-* Nick Galko. at http://https://galek.github.io/portfolio/
+* Nick Galko. at https://galek.github.io/portfolio/
 */
 /**
  * constant that contains secret key for jwt
